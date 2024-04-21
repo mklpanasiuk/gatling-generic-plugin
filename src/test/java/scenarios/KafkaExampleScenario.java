27ab347ed0e63c048b8979a2051eb0751ddd4486
valeriy.butorin <valeriy.butorin@tui.be>
2024-04-21 19:10:27 +0300

    misc refactoring, splitting scenarios

---
+++ src/test/java/scenarios/KafkaExampleScenario.java
@@ -0,0 +1,44 @@
+package scenarios;
+
+import io.gatling.javaapi.core.ScenarioBuilder;
+import io.gatling.javaapi.core.Simulation;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+import java.util.Properties;
+
+import static gatling.generic.plugin.GenericDsl.genericAction;
+import static io.gatling.javaapi.core.CoreDsl.pause;
+import static io.gatling.javaapi.core.CoreDsl.scenario;
+
+
+public class KafkaExampleScenario extends Simulation {
+
+
+    public ScenarioBuilder mainScenario() {
+        Properties props = new Properties();
+        props.put("bootstrap.servers", "localhost:9092");
+        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+
+        var producer = new KafkaProducer<String, String>(props);
+        var topic = "my-topic";
+
+        return scenario("Kafka Example")
+                .exec(
+                        genericAction("message_1", session -> {
+                            producer.send(new ProducerRecord<>(topic, "key-my-1", "value-my-1"));
+
+                            return session;
+                        }))
+                .exec(
+                        genericAction("message_2", session -> {
+                            producer.send(new ProducerRecord<>(topic, "key-my-2", "value-my-2"));
+
+                            return session;
+                        }))
+                .pause(1);
+
+    }
+}
+
--- src/test/java/simulations/SimpleExample.java
+++ src/test/java/simulations/SimpleExample.java
@@ -1,40 +1,14 @@
 package simulations;
 
-import dto.User;
 import io.gatling.javaapi.core.ScenarioBuilder;
 import io.gatling.javaapi.core.Simulation;
+import scenarios.SimpleExampleScenario;
 
-import static gatling.generic.plugin.GenericDsl.genericAction;
 import static io.gatling.javaapi.core.CoreDsl.*;
 
 public class SimpleExample extends Simulation {
 
-    ScenarioBuilder scenario = scenario("FirstScenario")
-        .exec(
-            genericAction("step_1", session -> {
-                var user = new User();
-                user.id = User.provideId();
-                user.name = "Mykola";
-
-                session = session.set("payload_1", user);
-
-                EmulateWork(111);
-
-                return session;
-            }),
-            genericAction("step_2", session -> {
-                var payload = (User) session.get("payload_1");
-                System.out.println("===> " + payload.id);
-                System.out.println("===> " + payload.name);
-
-                EmulateWork(222);
-
-                session = session.set("latency", 555);
-
-                return session;
-            })
-                    .pause(1)
-        );
+    ScenarioBuilder scenario = new SimpleExampleScenario().mainScenario();
 
     {
         setUp(scenario.injectClosed(constantConcurrentUsers(1).during(10)));
